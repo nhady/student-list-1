f4551e6758f902245d2bcdb1c2093510223c3304
nhady <64534317+nhady@users.noreply.github.com>
2020-05-22 19:18:01 +0200

    Update student_age.py

--- simple_api/student_age.py
+++ simple_api/student_age.py
@@ -5,26 +5,14 @@ from flask import abort
 from flask import make_response
 from flask import request
 from flask import url_for
-from flask_httpauth import HTTPBasicAuth
 from flask import g, session, redirect, url_for
 from flask_simpleldap import LDAP
 import json
 import os
 
-auth = HTTPBasicAuth()
+
 app = Flask(__name__)
 app.debug = True
-
-@auth.get_password
-def get_password(username):
-    if username == 'toto':
-        return 'python'
-    return 'python'
-
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
-
 
 try:
     student_age_file_path
@@ -40,7 +28,6 @@ def get_student_ages():
     return jsonify({'student_ages': student_age })
 
 @app.route('/pozos/api/v1.0/get_student_ages/<student_name>', methods=['GET'])
-#@auth.login_required
 def get_student_age(student_name):
     if student_name not in student_age :
         abort(404)
@@ -50,6 +37,7 @@ def get_student_age(student_name):
       with open(student_age_file_path, 'w') as student_age_file:
         json.dump(student_age, student_age_file, indent=4, ensure_ascii=False)
     return age 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
