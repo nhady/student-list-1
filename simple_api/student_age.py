8e9d47ff8df3b0ee1c8bef265f065f2e014c9367
nhady <64534317+nhady@users.noreply.github.com>
2020-05-22 22:22:42 +0200

    Update student_age.py

--- simple_api/student_age.py
+++ simple_api/student_age.py
@@ -43,4 +43,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port='5000')
