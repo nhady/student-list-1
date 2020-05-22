d6d620346b0594121811cf1637ebb15c224d7faa
nhady <64534317+nhady@users.noreply.github.com>
2020-05-22 22:43:07 +0200

    Update student_age.py

--- simple_api/student_age.py
+++ simple_api/student_age.py
@@ -12,10 +12,8 @@ import os
 
 
 app = Flask(__name__)
-app.debug = True
 
 try:
-    student_age_file_path
     student_age_file_path  = os.environ['student_age_file_path'] 
 except NameError:
     student_age_file_path  = '/data/student_age.json'
@@ -27,20 +25,5 @@ student_age = json.load(student_age_file)
 def get_student_ages():
     return jsonify({'student_ages': student_age })
 
-@app.route('/pozos/api/v1.0/get_student_ages/<student_name>', methods=['GET'])
-def get_student_age(student_name):
-    if student_name not in student_age :
-        abort(404)
-    if student_name in student_age :
-      age = student_age[student_name]
-      del student_age[student_name]
-      with open(student_age_file_path, 'w') as student_age_file:
-        json.dump(student_age, student_age_file, indent=4, ensure_ascii=False)
-    return age 
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
